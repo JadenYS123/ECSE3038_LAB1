4e57e85bbec057fb1d942545b4b25e65487e4435
Jaden Young Sang <jadenys2002gmail.com>
2023-01-27 15:41:46 -0500

    Potential divider function added to code

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ def parallel(r):
 
 parallel([330, 1000, 2200])
 
+def potential_divider(j,r):
+    r_size = len(r)
+
+    if (r_size > 0):
+        Total_R = 0
+        for a in r:
+            Total_R = a + Total_R
+
+        for a in r:
+            VR = (j * a) / Total_R
+            print(VR)
+
+
+potential_divider (9, [3000, 1000])
+
+
+
 
 
 
